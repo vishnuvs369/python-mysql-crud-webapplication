129a0eb6153d5b3cff90f2de5e565897b6ea510e
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-09-18 16:24:42 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -5,7 +5,7 @@ mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'vs26101996'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'vs261****'
 app.config['MYSQL_DATABASE_DB'] = 'pythoncrud'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
